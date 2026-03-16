19e88f69f0260b2d5027b47f1a443a59e840984a
Fahedoo <fahedismaili32@gmail.com>
2026-03-16 15:15:10 +0100

    Deleted useless file "gameplay.js" and changed the script link to "main.js" in the index

--- Public/script/gameplay.js
+++
@@ -1,7 +0,0 @@
-import { startGame } from './game-core.js';
-
-startGame({
-  wasmImportPath: '../wasm.js',
-  spritePath: '../assets/sprites/rats/rat_cours.png',
-  canvasIds: ['ecranDeJeu', 'game'],
-});
